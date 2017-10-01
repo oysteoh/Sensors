a7418d847b045987bdd23fd006b16f87336330a0
Øystein Olai Heggen <oystein.heggen@live.no>
2017-10-01 12:15:52 +0200

    fix name

--- I2C_TempHum_Sensor/I2C_temp_hum_mini.py
+++ I2C_TempHum_Sensor/I2C_temp_hum_mini.py
@@ -92,7 +92,7 @@ class th02:
 		t_raw=bus.read_i2c_block_data(self.ADDRESS, self.TH02_REG_DATA_H,3)
 		if debug:
 			print(t_raw)
-		temperature = (t_raw[1]<<8|t_raw[2])>>4
+		temperature = (t_raw[0]<<8|t_raw[1])>>4
 		return (temperature/16.0)-24.0
 		
 	def getStatus(self):
