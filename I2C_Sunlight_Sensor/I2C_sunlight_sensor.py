377cce9bcf3469ea0edb5c8d5b6d1572e92a426b
Øystein Olai Heggen <oystein.heggen@live.no>
2017-10-01 13:05:39 +0200

    fix name

--- I2C_Sunlight_Sensor/I2C_sunlight_sensor.py
+++ I2C_Sunlight_Sensor/I2C_sunlight_sensor.py
@@ -223,7 +223,7 @@ class SI1145(object):
                 # self.writeParam(SI1145_PARAM_ALSIRADCMUX, SI1145_PARAM_ADCMUX_SMALLIR)
 
                 # Fastest clocks, clock div 1
-                self.writeParam(SI1145_PARAM_ALSIRADCGAIN, 0)
+                # self.writeParam(SI1145_PARAM_ALSIRADCGAIN, 0)
 
                 # Take 511 clocks to measure
                 self.writeParam(SI1145_PARAM_ALSIRADCOUNTER, SI1145_PARAM_ADCCOUNTER_511CLK)
