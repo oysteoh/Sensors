2c3400c0b14b4de021b5b9fff9f0512bb3fca3fc
Øystein Olai Heggen <oystein.heggen@live.no>
2017-10-01 13:07:10 +0200

    fix name

--- I2C_Sunlight_Sensor/I2C_sunlight_sensor.py
+++ I2C_Sunlight_Sensor/I2C_sunlight_sensor.py
@@ -235,7 +235,7 @@ class SI1145(object):
                 # self.writeParam(SI1145_PARAM_ALSVISADCGAIN, 0)
 
                 # Take 511 clocks to measure
-                self.writeParam(SI1145_PARAM_ALSVISADCOUNTER, SI1145_PARAM_ADCCOUNTER_511CLK)
+                # self.writeParam(SI1145_PARAM_ALSVISADCOUNTER, SI1145_PARAM_ADCCOUNTER_511CLK)
 
                 # in high range mode (not normal signal)
                 self.writeParam(SI1145_PARAM_ALSVISADCMISC, SI1145_PARAM_ALSVISADCMISC_VISRANGE)
