9e14cbb72bdce0efdaaf656087cdd9d27049bd43
Øystein Heggen <oystein.heggen@gmail.com>
2017-10-21 23:45:36 +0200

    new sunlight code

--- I2C_Sunlight_Sensor/I2C.py
+++ I2C_Sunlight_Sensor/I2C.py
@@ -93,7 +93,7 @@ class Device(object):
         self._address = address
         if i2c_interface is None:
             # Use pure python I2C interface if none is specified.
-            import smbus
+            #import smbus
             self._bus = smbus.SMBus(busnum)
         else:
             # Otherwise use the provided class to create an smbus interface.
