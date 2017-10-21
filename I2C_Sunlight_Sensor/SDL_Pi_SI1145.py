f54210b7e526477db1b1dce996d47e5b3d7bdd2f
Øystein Heggen <oystein.heggen@gmail.com>
2017-10-21 22:08:05 +0200

    destroying sunlight sensors

--- I2C_Sunlight_Sensor/SDL_Pi_SI1145.py
+++ I2C_Sunlight_Sensor/SDL_Pi_SI1145.py
@@ -193,22 +193,22 @@ class SDL_Pi_SI1145(object):
         def _load_calibration(self):
                 # /***********************************/
                 # Enable UVindex measurement coefficients!
-                self._device.writeRaw8(SI1145_REG_UCOEFF0, 0x29)
-                self._device.writeRaw8(SI1145_REG_UCOEFF1, 0x89)
-                self._device.writeRaw8(SI1145_REG_UCOEFF2, 0x02)
-                self._device.writeRaw8(SI1145_REG_UCOEFF3, 0x00)
+                self._device.write8(SI1145_REG_UCOEFF0, 0x29)
+                self._device.write8(SI1145_REG_UCOEFF1, 0x89)
+                self._device.write8(SI1145_REG_UCOEFF2, 0x02)
+                self._device.write8(SI1145_REG_UCOEFF3, 0x00)
 
                 # Enable UV sensor
                 self.writeParam(SI1145_PARAM_CHLIST, SI1145_PARAM_CHLIST_ENUV | SI1145_PARAM_CHLIST_ENALSIR | SI1145_PARAM_CHLIST_ENALSVIS | SI1145_PARAM_CHLIST_ENPS1)
 
                 # Enable interrupt on every sample
-                self._device.writeRaw8(SI1145_REG_INTCFG, SI1145_REG_INTCFG_INTOE)
-                self._device.writeRaw8(SI1145_REG_IRQEN, SI1145_REG_IRQEN_ALSEVERYSAMPLE)
+                self._device.write8(SI1145_REG_INTCFG, SI1145_REG_INTCFG_INTOE)
+                self._device.write8(SI1145_REG_IRQEN, SI1145_REG_IRQEN_ALSEVERYSAMPLE)
 
                 # /****************************** Prox Sense 1 */
 
                 # Program LED current
-                self._device.writeRaw8(SI1145_REG_PSLED21, 0x03) # 20mA for LED 1 only
+                self._device.write8(SI1145_REG_PSLED21, 0x03) # 20mA for LED 1 only
                 self.writeParam(SI1145_PARAM_PS1ADCMUX, SI1145_PARAM_ADCMUX_LARGEIR)
 
                 # Prox sensor #1 uses LED #1
@@ -247,10 +247,10 @@ class SDL_Pi_SI1145(object):
                 self.writeParam(SI1145_PARAM_ALSVISADCMISC, 0)
 
                 # measurement rate for auto
-                self._device.writeRaw8(SI1145_REG_MEASRATE0, 0xFF) # 255 * 31.25uS = 8ms
+                self._device.write8(SI1145_REG_MEASRATE0, 0xFF) # 255 * 31.25uS = 8ms
 
                 # auto run
-                self._device.writeRaw8(SI1145_REG_COMMAND, SI1145_PSALS_AUTO)
+                self._device.write8(SI1145_REG_COMMAND, SI1145_PSALS_AUTO)
 
         # returns the UV index * 100 (divide by 100 to get the index)
         def readUV(self):
