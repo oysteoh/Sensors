53c85bd6d81c30b42f852e69779c34706b8bd112
Øystein Heggen <oystein.heggen@gmail.com>
2017-10-21 22:54:08 +0200

    new sunlight code

--- I2C_Sunlight_Sensor/SDL_Pi_SI1145.py
+++ I2C_Sunlight_Sensor/SDL_Pi_SI1145.py
@@ -240,7 +240,7 @@ class SDL_Pi_SI1145(object):
                 # not working self.writeParam(SI1145_PARAM_ALSVISADCGAIN, 4)
 
                 # Take 511 clocks to measure
-                self.writeParam(SI1145_PARAM_ALSVISADCOUNTER, SI1145_PARAM_ADCCOUNTER_511CLK)
+                # not working self.writeParam(SI1145_PARAM_ALSVISADCOUNTER, SI1145_PARAM_ADCCOUNTER_511CLK)
 
                 # in high range mode (not normal signal)
                 #self.writeParam(SI1145_PARAM_ALSVISADCMISC, SI1145_PARAM_ALSVISADCMISC_VISRANGE)
