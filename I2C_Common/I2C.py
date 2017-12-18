94070c201946267e6764b3acfa1678a8af4bec8f
Øystein Heggen <oystein.heggen@gmail.com>
2017-12-18 20:27:16 +0100

    added new folder

--- I2C_Common/I2C.py
+++ I2C_Common/I2C.py
@@ -40,16 +40,12 @@ def get_default_bus():
     Raspberry Pi either bus 0 or 1 (based on the Pi revision) will be returned.
     For a Beaglebone Black the first user accessible bus, 1, will be returned.
     """
-    plat = Platform.platform_detect()
-    if plat == Platform.RASPBERRY_PI:
-        if Platform.pi_revision() == 1:
-            # Revision 1 Pi uses I2C bus 0.
-            return 0
-        else:
-            # Revision 2 Pi uses I2C bus 1.
-            return 1
-    elif plat == Platform.BEAGLEBONE_BLACK:
-        # Beaglebone Black has multiple I2C buses, default to 1 (P9_19 and P9_20).
+    
+    if Platform.pi_revision() == 1:
+        # Revision 1 Pi uses I2C bus 0.
+        return 0
+    else:
+        # Revision 2 Pi uses I2C bus 1.
         return 1
     else:
         raise RuntimeError('Could not determine default I2C bus for platform.')
